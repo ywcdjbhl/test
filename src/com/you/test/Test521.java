198658721d938d53c0cde8756342c0a547b9fb89
ywcdjbhl <katejide@163.com>
2019-07-16 11:48:37 +0800

    test522

---
+++ src/com/you/test/Test521.java
@@ -0,0 +1,15 @@
+package com.you.test;
+
+public class Test521 {
+    public static void main(String[] ars) {
+        int i = 0;
+        try {
+            i = 2;
+            float f = 3 / 0;
+            i = 3;
+        } finally {
+            i = 5;
+        }
+        System.err.println(i);
+    }
+}
