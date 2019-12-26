674894e4197c0fe46dae60646b32ed1a184fd1fd
lidong <dong.li@scienjoy.com>
2019-12-26 18:15:51 +0800

    MapFactoryBean

---
+++ src/com/you/test/Test590.java
@@ -0,0 +1,10 @@
+package com.you.test;
+
+import org.springframework.beans.factory.config.MapFactoryBean;
+// MapFactoryBean
+public class Test590 {
+    public static void main(String[] args) {
+        MapFactoryBean bean = new MapFactoryBean();
+        bean.setTargetMapClass(null);
+    }
+}
