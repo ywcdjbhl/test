7725fab9d295b934df06913dcd277b4b607d459f
lidong <dong.li@scienjoy.com>
2020-01-03 17:32:48 +0800

    Arg.notNull

---
+++ src/com/you/test/Test609.java
@@ -0,0 +1,7 @@
+package com.you.test;
+
+public class Test609 {
+    public static void main(String[] args) {
+        org.apache.http.util.Args.notNull("banban", "lunch food");
+    }
+}
---
+++ src/com/you/test/Test610.java
@@ -0,0 +1,8 @@
+package com.you.test;
+
+public class Test610 {
+    public static void main(String[] args) {
+        String what = null;
+        org.apache.http.util.Args.notNull(what, "lunch food");
+    }
+}
