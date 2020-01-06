e524c214faebeccf185739d134e7b15d3c442442
lidong <dong.li@scienjoy.com>
2020-01-06 17:32:08 +0800

    String..args

---
+++ src/com/you/test/Test612.java
@@ -0,0 +1,8 @@
+package com.you.test;
+
+public class Test612 {
+    public Test612(String... args) {
+        System.err.println(args);
+        System.err.println(args.length);
+    }
+}
---
+++ src/com/you/test/Test613.java
@@ -0,0 +1,8 @@
+package com.you.test;
+
+public class Test613 {
+    public static void main(String[] args) {
+        new Test612();
+
+    }
+}
