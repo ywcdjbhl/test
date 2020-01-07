da5edf3aa4558d733acdbf7f3cd12fa5a3b6422a
lidong <dong.li@scienjoy.com>
2020-01-07 11:02:56 +0800

    DefaultResourceLoader

---
+++ src/com/you/test/Test615.java
@@ -0,0 +1,10 @@
+package com.you.test;
+
+import org.springframework.core.io.DefaultResourceLoader;
+
+public class Test615 {
+    public static void main(String[] args) {
+        DefaultResourceLoader loader = new DefaultResourceLoader();
+        loader.getResource(null);
+    }
+}
