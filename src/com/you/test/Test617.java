ba6bbe95cf324f733c92b668e92542165927b17a
lidong <dong.li@scienjoy.com>
2020-01-07 11:29:04 +0800

    ClassPathResource

---
+++ src/com/you/test/Test617.java
@@ -0,0 +1,11 @@
+package com.you.test;
+
+import org.springframework.core.io.ClassPathResource;
+
+public class Test617 {
+    public static void main(String[] args) {
+        String s = null;
+        ClassLoader cl = null;
+        new ClassPathResource(s, cl);
+    }
+}
