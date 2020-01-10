f48fd4359f3016ac1ffe87fbb7916afee7b210fa
lidong <dong.li@scienjoy.com>
2020-01-10 15:46:58 +0800

    PropertyPlaceholderHelper

---
+++ src/com/you/test/Test624.java
@@ -0,0 +1,11 @@
+package com.you.test;
+
+import org.springframework.util.PropertyPlaceholderHelper;
+
+// PropertyPlaceholderHelper
+
+public class Test624 {
+    public static void main(String[] args) {
+        new PropertyPlaceholderHelper("${", "}", ":", true);
+    }
+}
