d49cef10ba33389a3bc7522092778a0d2f74dbd6
lidong <dong.li@scienjoy.com>
2020-02-25 17:31:27 +0800

    ArrayListMultimap

---
+++ src/com/you/test/Test669.java
@@ -0,0 +1,10 @@
+package com.you.test;
+
+import com.google.common.collect.ArrayListMultimap;
+
+// ArrayListMultimap
+public class Test669 {
+    public static void main(String[] args) {
+        ArrayListMultimap.<String, Double>create();
+    }
+}
---
+++ src/com/you/test/Test670.java
@@ -0,0 +1,11 @@
+package com.you.test;
+
+import com.google.common.collect.ArrayListMultimap;
+
+// ArrayListMultimap
+public class Test670 {
+    public static void main(String[] args) {
+        ArrayListMultimap map = ArrayListMultimap.<String, Double>create();
+        map.put("f", 1D);
+    }
+}
