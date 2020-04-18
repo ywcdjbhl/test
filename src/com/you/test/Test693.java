3cc9aa99065d0311cd69036efd7b29c93e8007d2
lidong <dong.li@scienjoy.com>
2020-04-18 16:54:35 +0800

    clazz.getSimpleName()

---
+++ src/com/you/test/Test693.java
@@ -0,0 +1,10 @@
+package com.you.test;
+
+public class Test693 {
+    public static void main(String[] args) {
+        Class clazz = Test693.class;
+        System.err.println(clazz);
+        String name = clazz.getSimpleName();
+        System.err.println(name);
+    }
+}
