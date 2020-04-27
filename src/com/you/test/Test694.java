4c7baae891cab45d9a444272adf7d39ce6ac8f49
lidong <dong.li@scienjoy.com>
2020-04-27 10:30:01 +0800

    Assert

---
+++ src/com/you/test/Test694.java
@@ -0,0 +1,12 @@
+package com.you.test;
+
+import org.springframework.util.Assert;
+
+import java.util.Collection;
+
+public class Test694 {
+    public static void main(String[] args) {
+        Collection coll = null;
+        Assert.notEmpty(coll);
+    }
+}
