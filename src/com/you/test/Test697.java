84c04938d0cb6e8a18e09a53f8adc091af9c9ba3
lidong <dong.li@scienjoy.com>
2020-04-28 19:48:43 +0800

    ServiceLoader

---
+++ src/com/you/test/Test697.java
@@ -0,0 +1,9 @@
+package com.you.test;
+
+import java.util.ServiceLoader;
+// ServiceLoader
+public class Test697 {
+    public static void main(String[] args) {
+        ServiceLoader.load(null, null);
+    }
+}
