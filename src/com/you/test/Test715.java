679f8f734dcacd29eaa13c7e602723b55d019c1c
lidong <dong.li@scienjoy.com>
2020-05-06 16:28:38 +0800

    Optional

---
+++ src/com/you/test/Test715.java
@@ -0,0 +1,11 @@
+package com.you.test;
+
+import java.util.Optional;
+
+// Optional
+public class Test715 {
+    public static void main(String[] args) {
+        Optional op = Optional.empty();
+        System.err.println(op);
+    }
+}
