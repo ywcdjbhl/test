2ee753e6b99678dba4c322ba48080a5d79a33098
lidong <dong.li@scienjoy.com>
2020-05-06 16:40:08 +0800

    Optional.get()

---
+++ src/com/you/test/Test716.java
@@ -0,0 +1,11 @@
+package com.you.test;
+
+import java.util.Optional;
+
+// Optional
+public class Test716 {
+    public static void main(String[] args) {
+        Optional op = Optional.empty();
+        op.get();
+    }
+}
