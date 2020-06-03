74b80d8d6497214bb0ff64a0d6ff03102bdc523e
lidong <dong.li@scienjoy.com>
2020-06-03 11:33:55 +0800

    Arrays.fill

---
+++ src/com/you/test/Test739.java
@@ -0,0 +1,10 @@
+package com.you.test;
+
+import java.util.Arrays;
+// Arrays.fill
+public class Test739 {
+    public static void main(String[] args) {
+        int[] spine = new int[10];
+        Arrays.fill(spine, -1);
+    }
+}
---
+++ src/com/you/test/Test740.java
@@ -0,0 +1,10 @@
+package com.you.test;
+
+import java.util.Arrays;
+// Arrays.fill
+public class Test740 {
+    public static void main(String[] args) {
+        Object[] objects = new Object[15];
+        Arrays.fill(objects, 0, 10, "play");
+    }
+}
